e2224cf160416e7b27b95d53f69fc2d62f29908b
Alfredo <manuel.martinez96@hotmail.com>
2019-04-15 14:04:06 -0700

    first commit

---
+++ server/models/dummyUser.js
@@ -0,0 +1,14 @@
+module.exports = {
+    username: 'user123',
+    password: '1234567',
+    firstName: 'Jon',
+    lastName: 'Doe',
+    dob: '12/11/1991',
+    email: 'test@sgbsal.com',
+    address: {
+        street: '555 Bayshore Blvd',
+        city: 'Tampa',
+        state: 'Florida',
+        zip: '33813' 
+    }
+};
